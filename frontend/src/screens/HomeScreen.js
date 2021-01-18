589804f8c5a9bbba306963ae7db62e707a4c2012
Angie Rodriguez <angrodri902@gmail.com>
2021-01-17 17:22:08 -0800

    Modify HomeScreen with react-redux hooks

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Product from "../components/Product";
 import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
 
 const useStyles = makeStyles({
   root: {
@@ -12,37 +13,40 @@ const useStyles = makeStyles({
 });
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   const classes = useStyles();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get("/api/products");
-
-      setProducts(res.data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <Grid container spacing={2} className={classes.root}>
       <Grid container item>
         <Typography>Latest Products</Typography>
       </Grid>
-      {products.map((product) => (
-        <Grid
-          container
-          item
-          xs={12}
-          sm={6}
-          md={3}
-          lg={3}
-          xl={3}
-          key={product._id}
-        >
-          <Product product={product} />
-        </Grid>
-      ))}
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        products.map((product) => (
+          <Grid
+            container
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            lg={3}
+            xl={3}
+            key={product._id}
+          >
+            <Product product={product} />
+          </Grid>
+        ))
+      )}
     </Grid>
   );
 };
