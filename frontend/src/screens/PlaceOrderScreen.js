c30f76a49cf13adb2f03e8d7adb2bb9cdd57b7e2
Angie Rodriguez <angrodri902@gmail.com>
2021-03-01 10:53:39 -0800

    Create PlaceOrderScreen

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import CheckoutSteps from "../components/CheckoutSteps";
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+
+  const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+  // Calculate prices
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Grid container>
+        <Grid item md={8}>
+          <List>
+            <ListItem>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}
+                {cart.shippingAddress.postalCode} {cart.shippingAddress.country}
+              </p>
+            </ListItem>
+
+            <ListItem>
+              <h2>Payment Method</h2>
+
+              <strong>Method:</strong>
+              {cart.paymentMethod}
+            </ListItem>
+
+            <ListItem>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <List>
+                  {cart.cartItems.map((item, index) => (
+                    <ListItem key={index}>
+                      <Grid container>
+                        <Grid container item xs={5} sm={3} md={1}>
+                          <img src={item.image} alt={item.name} />
+                        </Grid>
+                        <Grid item md={1}>
+                          <Link to={`/product/${item.product}`} />
+                        </Grid>
+                        <Grid item md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Grid>
+                        <Grid item md={4}>
+                          <Card>
+                            {item.qty} x ${item.price} = $
+                            {item.qty * item.price}
+                          </Card>
+                        </Grid>
+                      </Grid>
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default PlaceOrderScreen;
